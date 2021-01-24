a06779f03597010e1f9649d6862477572f602ac5
Shamil Omarov <s_omarov05@mail.ru>
2021-01-24 23:34:11 +0300

    Рефакторинг кода

--- js/main.js
+++ js/main.js
@@ -1,50 +1,35 @@
-// let textArea = document.querySelector('#in');
+// ШИФРОВКА
+let encryptionText = document.querySelector('#encryption');
 
-// textArea.addEventListener('click', function() {
-//     let help = document.querySelector('#help');
-    // help.innerHTML = 123456;
-// });
-
-
-// шифровка
-document.querySelector('#encryption').oninput = function() {
+encryptionText.addEventListener('input', function({ target }) {
     // смещение
     const offset = 3;
-    // let help = document.querySelector('#help');
-
-    // из кода в символ
-    // help.innerHTML = String.fromCharCode(97);
-
-    // получаем строку
-    let str = this.value;
-
-    // из символа в код
-    console.log(str.charCodeAt(0));
 
     // выходная строка
     let out = '';
 
     // в цикле перебираем строку
-    for (let i = 0; i < str.length; i++) {
-        let code = str.charCodeAt(i);
+    for (let i = 0; i < target.value.length; i++) {
+        let code = target.value.charCodeAt(i);
         code += offset;
         // преобразовываем в символы
         out += String.fromCharCode(code);
     }
     // выводим строку в новый элемент
-    document.querySelector('#out').innerHTML = out;
-}
+    document.querySelector('#encryption-out').innerHTML = out;
+});
 
-// расшифровка
-document.querySelector('#transcript').oninput = function() {
+// РАСШИФРОВКА
+let transcriptText = document.querySelector('#transcript');
+
+transcriptText.addEventListener('input', function({ target }) {
     const offset = 3;
-    let str = this.value;
     let out = '';
 
-    for (let i = 0; i < str.length; i++) {
-        let code = str.charCodeAt(i);
+    for (let i = 0; i < target.value.length; i++) {
+        let code = target.value.charCodeAt(i);
         code -= offset;
         out += String.fromCharCode(code);
     }
-    document.querySelector('#out2').innerHTML = out;
-}
+    document.querySelector('#transcript-out').innerHTML = out;
+});
